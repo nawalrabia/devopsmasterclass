4e67e6727705970763f81fb3d27696eafbea255c
nawal rabia <nawalrabiajazad@gmail.com>
2026-05-06 15:43:46 +0100

    updated welcome page.

--- obj/Debug/net10.0/mywebapp.AssemblyInfo.cs
+++ obj/Debug/net10.0/mywebapp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("mywebapp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee0a68012e32ccaf6c1ba2169e4c978482fc1952")]
 [assembly: System.Reflection.AssemblyProductAttribute("mywebapp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("mywebapp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
